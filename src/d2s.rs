0d88185ad5d6bcd18c64399d1ae90ae15f2fe5be
feored <feordo@gmail.com>
2023-07-18 03:19:49 +0200

    add checksum algorithm + saving file

--- src/d2s.rs
+++ src/d2s.rs
@@ -16,9 +16,9 @@ pub enum HeaderID {
     CharacterProgression,
 }
 
-pub fn get_header_bytes_range(id:HeaderID) -> (usize, usize){
+pub fn get_header_bytes_range(id: HeaderID) -> (usize, usize) {
     let header_data: HeaderSection = get_header_data(id);
-    (header_data.offset, header_data.offset+header_data.bytes)
+    (header_data.offset, header_data.offset + header_data.bytes)
 }
 
 fn get_header_data(id: HeaderID) -> HeaderSection {
@@ -64,22 +64,43 @@ pub enum Version {
     v107,
     v108,
     v109,
-    v110
+    v110,
 }
 
-pub fn get_version(version_bytes : &[u8;4]) -> Result<Version, &'static str> {
-    let version_number : u32 = u32::from_le_bytes(*version_bytes);
+pub fn get_version(version_bytes: &[u8; 4]) -> Result<Version, &'static str> {
+    let version_number: u32 = u32::from_le_bytes(*version_bytes);
     match version_number {
-        71  => Ok(Version::v100),
-        87  => Ok(Version::v107),
-        89  => Ok(Version::v108),
-        92  => Ok(Version::v109),
-        96  => Ok(Version::v110),
-        _   => Err("version ID does not match any known version of the game.")
+        71 => Ok(Version::v100),
+        87 => Ok(Version::v107),
+        89 => Ok(Version::v108),
+        92 => Ok(Version::v109),
+        96 => Ok(Version::v110),
+        _ => Err("version ID does not match any known version of the game."),
     }
 }
 
 pub fn check_valid_signature(bytes: &Vec<u8>) -> bool {
     let (header_start, header_end) = get_header_bytes_range(HeaderID::Signature);
     bytes[header_start..header_end] == D2S_SIGNATURE
+}
+
+// checksum = 0
+//     for i in range(len(data)):
+//         ch = data[i]
+//         if (i >= OFFSET_CHECKSUM_START and i < OFFSET_CHECKSUM_END):
+//             ch = 0
+//         checksum = ctypes.c_int32((checksum << 1) + ch + ctypes.c_int32(checksum < 0).value).value
+//     return checksum
+
+pub fn calc_checksum(bytes: &Vec<u8>) -> i32{
+    let mut checksum : i32 = 0;
+    let (checksum_start, checksum_end) = get_header_bytes_range(HeaderID::Checksum);
+    for i in 0..bytes.len(){
+        let mut ch : i32 = bytes[i] as i32;
+        if i >= checksum_start && i < checksum_end {
+            ch = 0;
+        }
+        checksum = (checksum << 1) + ch + ((checksum < 0) as i32);
+    }
+    checksum
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::error::Error;
+use std::io::Write;
 use std::path::Path;
-
-use crate::d2s::get_version;
+use std::fs::OpenOptions;
+use std::cmp;
 
 mod d2s;
 
@@ -15,27 +17,63 @@ fn get_header_value(bytes: &Vec<u8>, id: d2s::HeaderID) -> Vec<u8> {
     header_value
 }
 
+
+fn save_file(path : &Path, data : Vec<u8>) -> std::io::Result<()>{
+    let mut file: std::fs::File = OpenOptions::new().read(true).write(true).open(path)?;
+    file.write_all(&data)
+}
+
 fn main() {
     let path: &Path = Path::new(CHARACTER_FILE);
-    let save_file: Vec<u8> = match std::fs::read(path) {
+    let mut file_contents: Vec<u8> = match std::fs::read(path) {
         Ok(bytes) => bytes,
-        Err(e) => panic!("File invalid: {e:?}"),
+        Err(e) => panic!("File invalid: {e:?}")
     };
-    let valid_file: bool = d2s::check_valid_signature(&save_file);
+    let valid_file: bool = d2s::check_valid_signature(&file_contents);
     println!("This file is valid : {}", valid_file);
 
-    let character_name: Vec<u8> = get_header_value(&save_file, d2s::HeaderID::CharacterName);
+    let character_name: Vec<u8> = get_header_value(&file_contents, d2s::HeaderID::CharacterName);
     let character_name_as_string = String::from_utf8(character_name);
-    match character_name_as_string{
+    match character_name_as_string {
         Ok(c_name) => println!("Character name: '{c_name}'"),
-        Err(e) => eprintln!("Error reading character name: {e:?}")
+        Err(e) => eprintln!("Error reading character name: {e:?}"),
     }
-    let mut version_bytes: [u8;4] = Default::default();
-    version_bytes.copy_from_slice(&get_header_value(&save_file, d2s::HeaderID::VersionID)[0..4]);
+    
+
+    // // Change name
+    // let (name_start, name_end) = d2s::get_header_bytes_range(d2s::HeaderID::CharacterName);
+    // let mut new_name_bytes: [u8;16] = [0; 16]; 
+    // let new_name = "George".as_bytes();
+    // for i in 0..new_name.len(){
+    //     new_name_bytes[i] = new_name[i];
+    // }
+    // for i in name_start..name_end{
+    //     file_contents[i] = new_name_bytes[i - name_start];
+    // }
+    // // Note: if file name != character name, file won't open
+
+
+    let mut version_bytes: [u8; 4] = Default::default();
+    version_bytes.copy_from_slice(&get_header_value(&file_contents, d2s::HeaderID::VersionID)[0..4]);
     let version: Result<d2s::Version, &str> = d2s::get_version(&version_bytes);
-    match version{
+    match version {
         Ok(version) => println!("Detected Version: '{version:?}'"),
-        Err(e) => eprintln!("Error reading version: {e:?}")
+        Err(e) => eprintln!("Error reading version: {e:?}"),
+    }
+
+    let checksum_bytes: [u8; 4] = d2s::calc_checksum(&file_contents).to_le_bytes();
+    println!("Checksum: {:X?}", checksum_bytes);
+
+
+
+    // replace checksum
+    let (checksum_start, checksum_end) = d2s::get_header_bytes_range(d2s::HeaderID::Checksum);
+    for i in checksum_start..checksum_end{
+        file_contents[i] = checksum_bytes[i - checksum_start];
+    }
+    match save_file(path, file_contents){
+        Ok(()) => println!("Successfully saved file: {path:?}"),
+        Err(e) => eprintln!("Error while saving file : {e:?}")
     }
 
 }
