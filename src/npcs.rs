9134f63c5103a81b7c57e2b826875faa7e960480
feored <feordo@gmail.com>
2023-07-23 03:13:19 +0200

    Read and write skills.

--- src/npcs.rs
+++ src/npcs.rs
@@ -1,11 +1,11 @@
-pub const OFFSET : usize = 714;
-const HEADER : [u8;3] = [0x77, 0x34, 0x00];
-const NPCS_LENGTH : usize = 40;
+pub const OFFSET: usize = 714;
+const HEADER: [u8; 3] = [0x77, 0x34, 0x00];
+const NPCS_LENGTH: usize = 40;
 
-pub fn build_section() -> Vec<u8>{
-    let mut section = vec!();
+pub fn build_section() -> Vec<u8> {
+    let mut section = vec![];
     section.extend_from_slice(&HEADER);
-    for _i in 0..NPCS_LENGTH{
+    for _i in 0..NPCS_LENGTH {
         section.push(0x00);
     }
     section
